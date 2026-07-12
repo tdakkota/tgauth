72b520425efaed98f38e385b3d5f5171525451b1
tdakkota <github@tdakkota.dev>
2026-07-12 13:52:03 +0300

    feat(qr): add --password and --password-stdin for 2FA support

--- qr.go
+++ qr.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/go-faster/errors"
 	"github.com/gotd/td/telegram"
 	"github.com/gotd/td/telegram/auth/qrlogin"
 	"github.com/gotd/td/tg"
+	"github.com/gotd/td/tgerr"
 	"github.com/mdp/qrterminal/v3"
 	"github.com/spf13/cobra"
 	"go.uber.org/multierr"
@@ -17,9 +20,11 @@ import (
 
 func qrCmd() *cobra.Command {
 	var (
-		pngPath    string
-		gotdFlags  gotdOptions
-		printFlags printOptions
+		pngPath       string
+		password      string
+		passwordStdin bool
+		gotdFlags     gotdOptions
+		printFlags    printOptions
 	)
 
 	cmd := &cobra.Command{
@@ -27,6 +32,14 @@ func qrCmd() *cobra.Command {
 		Short: "Create session via QR login flow",
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			ctx := cmd.Context()
+
+			if passwordStdin {
+				data, err := io.ReadAll(os.Stdin)
+				if err != nil {
+					return errors.Wrap(err, "read password from stdin")
+				}
+				password = strings.TrimSpace(string(data))
+			}
 
 			d := tg.NewUpdateDispatcher()
 			loggedIn := qrlogin.OnLoginToken(d)
@@ -58,8 +71,18 @@ func qrCmd() *cobra.Command {
 					}
 
 					_, err := client.QR().Auth(ctx, loggedIn, show)
-					if err != nil {
+					if err == nil {
+						return nil
+					}
+					if !tgerr.Is(err, "SESSION_PASSWORD_NEEDED") {
 						return errors.Wrap(err, "QR login")
+					}
+					if password == "" {
+						return errors.New("2FA password required but not provided; use --password or --password-stdin")
+					}
+					_, err = client.Auth().Password(ctx, password)
+					if err != nil {
+						return errors.Wrap(err, "password login")
 					}
 					return nil
 				},
@@ -73,6 +96,8 @@ func qrCmd() *cobra.Command {
 	}
 
 	cmd.Flags().StringVar(&pngPath, "png", "", "Generate path to image of QR")
+	cmd.Flags().StringVar(&password, "password", "", "2FA password")
+	cmd.Flags().BoolVar(&passwordStdin, "password-stdin", false, "Read 2FA password from stdin")
 	gotdFlags.install(cmd.Flags())
 	printFlags.install(cmd.Flags())
 
